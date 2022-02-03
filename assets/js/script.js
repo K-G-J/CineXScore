a92ad133ff25ba78ec877ffa8a3613d8d4cd7fc0
mndrs <mndrs.kc@gmail.com>
2022-02-03 13:12:38 -0500

    styling CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-var sendTitle = function() {
+var sendTitle = function () {
   var title = $("input[name='movie-search-title']").val();
-    getMovie(title)
-    getQuotes(title)
+  getMovie(title)
+  getQuotes(title)
 }
-$("input[name='movie-search-title']").keydown(function (e){
-  if(e.keyCode == 13){
+$("input[name='movie-search-title']").keydown(function (e) {
+  if (e.keyCode == 13) {
     e.preventDefault();
     sendTitle();
   }
@@ -13,18 +13,18 @@ $("input[name='movie-search-title']").keydown(function (e){
 var searchInput = document.getElementById('searchBox');
 var baseUrl = "http://sg.media-imdb.com/suggests/";
 var resultText = document.getElementById('result');
-searchInput.addEventListener('keyup', function(){
+searchInput.addEventListener('keyup', function () {
   $("#result").removeClass("hidden");
-	//clearing blank spaces from input
-	var cleanInput = searchInput.value.replace(/\s/g, "");
-	//clearing result div if the input box in empty
-	if(cleanInput.length === 0) {
-		resultText.innerHTML = "";
-	}
-	if(cleanInput.length > 0) {
-		var queryUrl = baseUrl + cleanInput[0].toLowerCase() + "/" 
-    + cleanInput.toLowerCase()
-    + ".json";	
+  //clearing blank spaces from input
+  var cleanInput = searchInput.value.replace(/\s/g, "");
+  //clearing result div if the input box in empty
+  if (cleanInput.length === 0) {
+    resultText.innerHTML = "";
+  }
+  if (cleanInput.length > 0) {
+    var queryUrl = baseUrl + cleanInput[0].toLowerCase() + "/"
+      + cleanInput.toLowerCase()
+      + ".json";
     $.ajax({
       url: queryUrl,
       dataType: 'jsonp',
@@ -33,88 +33,88 @@ searchInput.addEventListener('keyup', function(){
       jsonpCallback: "imdb$" + cleanInput.toLowerCase()
     }).done(function (result) {
       //clearing result div if there is a valid response
-      if(result.d.length > 0) {
+      if (result.d.length > 0) {
         resultText.innerHTML = "";
       }
-      for(var i = 0; i < result.d.length; i++) {
-        let category = result.d[i].id.slice(0,2);
-        if(category === "tt" || category === "nm") {		    		
+      for (var i = 0; i < result.d.length; i++) {
+        let category = result.d[i].id.slice(0, 2);
+        if (category === "tt" || category === "nm") {
           //row for risplaying one result
           let resultRow = document.createElement('p');
           resultRow.setAttribute('class', 'resultRow')
-		    		//creating and setting description
-            let description = document.createElement('div');
-            description.setAttribute('class', 'description');
-            let name = document.createElement('h4');
-            let snippet = document.createElement('h5');
-            if(category === "tt" && result.d[i].y) {
-              name.innerHTML = result.d[i].l + " (" + result.d[i].y + ")";
-              let nameText = name.innerHTML
-              $(name).click(function (e) { 
-                e.preventDefault();
-                let title = nameText.slice(0, nameText.lastIndexOf(" "))
-                getMovie(title)
-                getQuotes(title)
-              });
-            } else {
-              name.innerHTML = result.d[i].l;
-              let nameText = name.innerHTML
-              $(name).click(function (e) { 
-                e.preventDefault();
-                let title = nameText.slice(0, nameText.lastIndexOf(" "))
-                getMovie(title)
-                getQuotes(title)
-              });
-            }
-            snippet.innerHTML = result.d[i].s;
-            $(description).append(name);
-            $(resultRow).append(description);
-            $("#result").append(resultRow);
-		    	}
-		    }
-		});
-	}
+          //creating and setting description
+          let description = document.createElement('div');
+          description.setAttribute('class', 'description');
+          let name = document.createElement('h4');
+          let snippet = document.createElement('h5');
+          if (category === "tt" && result.d[i].y) {
+            name.innerHTML = result.d[i].l + " (" + result.d[i].y + ")";
+            let nameText = name.innerHTML
+            $(name).click(function (e) {
+              e.preventDefault();
+              let title = nameText.slice(0, nameText.lastIndexOf(" "))
+              getMovie(title)
+              getQuotes(title)
+            });
+          } else {
+            name.innerHTML = result.d[i].l;
+            let nameText = name.innerHTML
+            $(name).click(function (e) {
+              e.preventDefault();
+              let title = nameText.slice(0, nameText.lastIndexOf(" "))
+              getMovie(title)
+              getQuotes(title)
+            });
+          }
+          snippet.innerHTML = result.d[i].s;
+          $(description).append(name);
+          $(resultRow).append(description);
+          $("#result").append(resultRow);
+        }
+      }
+    });
+  }
 });
 
-var loadPastSearches = function() {
+var loadPastSearches = function () {
   var pastSearches = JSON.parse(localStorage.getItem("movieObjects"));
   if (!pastSearches || !Array.isArray(pastSearches)) return []
   else return pastSearches
 }
-var loadFaveTracks = function() {
+var loadFaveTracks = function () {
   var faveTracks = JSON.parse(localStorage.getItem("trackObjects"));
-  if(!faveTracks || !Array.isArray(faveTracks)) return []
+  if (!faveTracks || !Array.isArray(faveTracks)) return []
   else return faveTracks
 }
 // primary movie information (API #1)
-var getMovie = function(title) {
-    $("#result").addClass("hidden")
-    $("#main").removeClass("hidden");
-    $("#search-form").trigger("reset");
-    //format the OMDB api url 
-    var apiUrl = `http://www.omdbapi.com/?t=${title}&plot=full&apikey=836f8b0`
-    //make a request to the url 
-    fetch(apiUrl)
-    .then(function(response) {
-         // request was successful 
-        if (response.ok) {
-            response.json().then(function(movieData) {
-            // console.log(movieData)
-            var movieTitle = movieData.Title
-            getMovieId(movieTitle);
-            getSoundTrack(movieTitle);
-            getTrailer(movieTitle);
-            var movieObj = {
-              title: movieTitle,
-            }
-            let pastSearches = loadPastSearches();
-            let alreadySearched = false 
-            if (pastSearches) {
-              pastSearches.forEach (s => {
-                if (s.title === movieTitle) {
-                  alreadySearched = true;
-                  }
-              })
+var getMovie = function (title) {
+  $("#result").addClass("hidden")
+  $("#main").removeClass("hidden");
+  $("#search-form").trigger("reset");
+  //format the OMDB api url 
+  var apiUrl = `http://www.omdbapi.com/?t=${title}&plot=full&apikey=836f8b0`
+  //make a request to the url 
+  fetch(apiUrl)
+    .then(function (response) {
+      // request was successful 
+      if (response.ok) {
+        response.json().then(function (movieData) {
+          // console.log(movieData)
+          var movieTitle = movieData.Title
+          getMovieId(movieTitle);
+          getSoundTrack(movieTitle);
+          getTrailer(movieTitle);
+          var movieObj = {
+            title: movieTitle,
+          }
+          let pastSearches = loadPastSearches();
+          let alreadySearched = false
+          if (pastSearches) {
+            pastSearches.forEach(s => {
+              if (s.title === movieTitle) {
+                alreadySearched = true;
+              }
+            })
           }
           if (!alreadySearched) {
             saveSearch(movieObj)
@@ -124,27 +124,27 @@ var getMovie = function(title) {
               $(searchEl).text(pastSearchTitle)
               $(searchEl).addClass("past-search-item");
               $("#past-search-dropdown").append(searchEl)
-              $(searchEl).click(function (e) { 
+              $(searchEl).click(function (e) {
                 e.preventDefault();
                 let title = pastSearchTitle
                 getMovie(title)
                 getQuotes(title)
               });
-              }
             }
-            showMovie(movieData);
+          }
+          showMovie(movieData);
         });
-    } else {
+      } else {
         alert("Error: title not found!");
-    }
-})
-.catch(function(error) {
-    alert("Unable to connect to CineXScore app");
-    console.log(error)
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to CineXScore app");
+      console.log(error)
     });
 };
 // get specialID for other movie details (API #2)
-var getMovieId = function(movieTitle) {
+var getMovieId = function (movieTitle) {
   const settings = {
     "async": true,
     "crossDomain": true,
@@ -155,19 +155,19 @@ var getMovieId = function(movieTitle) {
       "x-rapidapi-key": "229d984177msh18d191b1335378fp137dcejsn7c92ab2acfaf"
     }
   };
-  
+
   $.ajax(settings).done(function (response) {
     console.log(response);
     var specialId = response.results[0].id
     var specialId = specialId.replace("/title/", "")
-    var specialId = specialId.replace("/","")
+    var specialId = specialId.replace("/", "")
     // console.log(specialId)
     getMovieImgs(specialId)
     getPromos(specialId)
   });
 }
 // get hero image (API #2)
-var getPromos = function(specialId) {
+var getPromos = function (specialId) {
   const settings = {
     "async": true,
     "crossDomain": true,
@@ -184,7 +184,7 @@ var getPromos = function(specialId) {
   });
 }
 // get additional images (API #2)
-var getMovieImgs = function(specialId) {
+var getMovieImgs = function (specialId) {
   const settings = {
     "async": true,
     "crossDomain": true,
@@ -195,14 +195,14 @@ var getMovieImgs = function(specialId) {
       "x-rapidapi-key": "229d984177msh18d191b1335378fp137dcejsn7c92ab2acfaf"
     }
   };
-  
+
   $.ajax(settings).done(function (movieImgs) {
     // console.log(movieImgs);
     $("#cast-image").attr("src", movieImgs.images[0].url);
   });
 }
 // get movie soundtrack (API #3)
-var getSoundTrack = function(movieTitle) {
+var getSoundTrack = function (movieTitle) {
   var movieTitle = movieTitle.replaceAll(" ", "")
   const settings = {
     "async": true,
@@ -214,7 +214,7 @@ var getSoundTrack = function(movieTitle) {
       "x-rapidapi-key": "229d984177msh18d191b1335378fp137dcejsn7c92ab2acfaf"
     }
   };
-  
+
   $.ajax(settings).done(function (soundTrackData) {
     console.log(soundTrackData);
     if (!Object.keys(soundTrackData).length) {
@@ -237,7 +237,7 @@ var getSoundTrack = function(movieTitle) {
         name: albumName,
         url: albumUrl
       };
-      $("#save-to-favorites").click(function (e) { 
+      $("#save-to-favorites").click(function (e) {
         e.preventDefault();
         saveTrack(trackObj)
       });
@@ -258,22 +258,22 @@ var getTrailer = function (movieTitle) {
       "x-rapidapi-key": "229d984177msh18d191b1335378fp137dcejsn7c92ab2acfaf"
     }
   };
-  
+
   $.ajax(settings).done(function (trailerData) {
     // console.log(trailerData);
     $("#video-title").text(trailerData.items[0].title)
-    var embedCode = trailerData.items[0].url.replace("https://www.youtube.com/watch?v=","")
+    var embedCode = trailerData.items[0].url.replace("https://www.youtube.com/watch?v=", "")
     var trailerLink = `https://www.youtube.com/embed/${embedCode}`
     $("#trailer").attr("src", trailerLink)
     $("#video-description").text(trailerData.items[0].description)
-    
+
   });
 }
 // get movie quotes (API #5)
-var getQuotes = function(title) {
+var getQuotes = function (title) {
   $("#quote-items").html("");
   $("#movie-quotes-heading").removeClass("hidden");
-  var title = title.replaceAll(" ","_")
+  var title = title.replaceAll(" ", "_")
   const settings = {
     "async": true,
     "crossDomain": true,
@@ -284,20 +284,20 @@ var getQuotes = function(title) {
       "x-rapidapi-key": "229d984177msh18d191b1335378fp137dcejsn7c92ab2acfaf"
     }
   };
-  
+
   $.ajax(settings).done(function (quoteData) {
     // console.log(quoteData);
     showQuotes(quoteData)
   })
-  .fail(function(xhr, status, error) {
-    //Ajax request failed.
-    var errorMessage = xhr.status + ': ' + xhr.statusText
-    console.log(`No quotes available for this query - ${errorMessage}`);
-    $("#movie-quotes-heading").addClass("hidden");
-  });
+    .fail(function (xhr, status, error) {
+      //Ajax request failed.
+      var errorMessage = xhr.status + ': ' + xhr.statusText
+      console.log(`No quotes available for this query - ${errorMessage}`);
+      $("#movie-quotes-heading").addClass("hidden");
+    });
 }
 // change the page HTML to show movie information
-var showMovie = function(movieData) {
+var showMovie = function (movieData) {
   $("#movie-title").text(movieData.Title)
   $("#year-rating").text(`${movieData.Year}, ${movieData.Rated}`)
   $("#genre").text(`${movieData.Genre}`)
@@ -318,9 +318,9 @@ var showMovie = function(movieData) {
   }
 }
 // if there are famous quotes display those on page 
-var showQuotes = function(quoteData) {
+var showQuotes = function (quoteData) {
   $("#movie-quotes-heading").text("Movie Quotes")
-    quoteData.forEach(quoteItem => {
+  quoteData.forEach(quoteItem => {
     var quoteText = document.createElement("div")
     $(quoteText).html(`<h4 class='quote'>"${quoteItem.quote}"<br><br><span class='quote-character'>-${quoteItem.character}</span></h4><br>`)
     $(quoteText).appendTo("#quote-items");
@@ -333,17 +333,17 @@ var saveSearch = function (movieObj) {
   localStorage.setItem("movieObjects", JSON.stringify(pastSearches))
 }
 // dropdown favorite soundtrack buttons 
-var saveTrack = function(trackObj) {
+var saveTrack = function (trackObj) {
   let faveTracks = loadFaveTracks();
-  let alreadySearched = false 
+  let alreadySearched = false
   if (faveTracks) {
-    faveTracks.forEach (t => {
+    faveTracks.forEach(t => {
       if (t.name === trackObj.name) {
         alreadySearched = true;
-        }
+      }
     })
-}
-if (!alreadySearched) {
+  }
+  if (!alreadySearched) {
     let trackEl = document.createElement("a")
     $(trackEl).addClass("fave-track");
     $(trackEl).text(trackObj.name);
@@ -363,7 +363,7 @@ var populateSearches = function () {
     $(searchEl).text(pastSearchTitle)
     $(searchEl).addClass("past-search-item");
     $("#past-search-dropdown").append(searchEl)
-    $(searchEl).click(function (e) { 
+    $(searchEl).click(function (e) {
       e.preventDefault();
       let title = pastSearchTitle
       getMovie(title)
@@ -374,7 +374,7 @@ var populateSearches = function () {
 populateSearches();
 
 // populate favorites list when page loads
-var populateFavorites = function() {
+var populateFavorites = function () {
   let faveTracks = loadFaveTracks();
   for (var track of faveTracks) {
     let trackEl = document.createElement("a")
@@ -384,15 +384,15 @@ var populateFavorites = function() {
     $(trackEl).attr("target", "_blank")
     $("#favorite-tracks-dropdown").append(trackEl)
   }
-} 
+}
 populateFavorites();
 // clear searches and favorite tracks
-$("#clear-searches").click(function (e) { 
+$("#clear-searches").click(function (e) {
   e.preventDefault();
   [...$(".past-search-item")].map(thisSearch => thisSearch.remove());
   localStorage.removeItem("movieObjects");
 });
-$("#clear-favorites").click(function (e) { 
+$("#clear-favorites").click(function (e) {
   e.preventDefault();
   [...$(".fave-track")].map(thisTrack => thisTrack.remove());
   localStorage.removeItem("trackObjects");
